719c94bc61a9887b0d9b0322c61a485452fa7b4c
League Student <league@iMac-12.attlocal.net>
2020-01-05 17:22:32 -0800

    gg
    gg

--- src/_02_gotta_catchem_all/ExceptionMethods.java
+++ src/_02_gotta_catchem_all/ExceptionMethods.java
@@ -1,5 +1,11 @@
 package _02_gotta_catchem_all;
 
 public class ExceptionMethods {
-	
+	public double divide(double numerator, double denominator) {
+		if(0.0 == denominator) {
+			throw new IllegalArgumentException();
+		}
+		return numerator/denominator;
+		
+	}
 }
